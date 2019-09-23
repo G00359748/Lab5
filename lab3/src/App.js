3394e4cc8c21deed80e9b73099bbba53b5e7276a
SHANE MC CORMACK <G00359748@gmit.ie>
2019-09-23 13:45:23 +0100

    first commit

---
+++ lab3/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import Content from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+class App extends React.Component {
+  render(){
+  return (
+    <Router>
+    <div className="App">
+      <Navbar bg="primary" variant="dark">
+    <Nav className="mr-auto">
+      <Nav.Link href="/read/">Read</Nav.Link>
+      <Nav.Link href="/create">Create</Nav.Link>
+      <Nav.Link href="/">Pricing</Nav.Link>
+    </Nav>
+  </Navbar>
+
+  <Switch>
+  <Route exact path="/" component={Content}/>
+  <Route path="/read" component={Header}/>
+  <Route path="/create" component={Footer}/>
+</Switch>
+
+
+     {/* <Header></Header>
+         <h1>Hello World</h1>
+          <h2>It is {new Date().toLocaleTimeString()}</h2> 
+          <br></br>
+          <Footer></Footer>
+     <Content></Content> */}
+          
+
+    </div>
+    </Router>
+  );
+}
+}
+
+export default App;
